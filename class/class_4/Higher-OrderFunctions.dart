30ee3c3177c45752f70cbbb1e02273ffc2518715
Razwan Ahammad <razwan.self@gmail.com>
2026-08-03 13:58:40 +0600

    Higher-Order Functions

---
+++ class/class_4/Higher-OrderFunctions.dart
@@ -0,0 +1,36 @@
+void main(){
+  // Higher-Order Functions
+  /* 
+  Lesson 5.1
+✅ Anonymous Functions
+
+Lesson 5.2
+✅ Functions as Variables
+
+Lesson 5.3
+✅ Callback Functions
+
+Lesson 5.4
+✅ forEach()
+
+Lesson 5.5
+✅ map()
+
+Lesson 5.6
+✅ where()
+
+Lesson 5.7
+✅ any()
+
+Lesson 5.8
+✅ every()
+
+Lesson 5.9
+✅ firstWhere()
+
+Lesson 5.10
+✅ sort()
+
+🎯 Mini Project
+  */
+}
