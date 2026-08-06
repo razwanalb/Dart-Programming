257aa32f1dbafbd726fcfefc50f96a412a142387
Razwan Ahammad <razwan.self@gmail.com>
2026-08-06 21:16:45 +0600

    Add Searching&Utilities

---
+++ class/class_5/Searching_Utilities.dart
@@ -0,0 +1,14 @@
+void main(){
+  
+  /*
+  # Searching
+- contains()  -> Check whether a value exists 
+- indexOf()   -> Find the index of a value 
+
+# Utilities
+- sort()      -> Sort the list in ascending order 
+- reversed    -> Reverse the list 
+
+  */
+print("hi");
+}
