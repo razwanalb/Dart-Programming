e4db8d5c0855fd68b944e3c3a3ce7ccf78b207b0
Razwan Ahammad <razwan.self@gmail.com>
2026-08-08 23:27:23 +0600

    for in Loop

--- class/class_5/for_inLoop.dart
+++ class/class_5/for_inLoop.dart
@@ -1,5 +1,8 @@
 void main(){
 
+  // The for-in loop is used to read/traverse each value of a collection one by one.
+  // for-in = Take every item from the List, one by one.
+
 List<String> names = [
   "Razwan",
   "Life",
