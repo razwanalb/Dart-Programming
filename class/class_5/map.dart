46052ed9a6864475081771c35f91ef221fdddb72
Razwan Ahammad <razwan.self@gmail.com>
2026-08-11 20:58:08 +0600

    Map Update

--- class/class_5/map.dart
+++ class/class_5/map.dart
@@ -7,14 +7,28 @@ void main(){
   key: value,
   
 */
-Map <String, dynamic> StudentInfo = {
-  "Name ":"Razwan ahammad",
+// Map <String, dynamic> StudentInfo = {
+//   "Name ":"Razwan ahammad",
+//   "Age" : 22,
+//   "Roll" : 93,
+//   "Department ":"CST",
+//   "CGPA" : 3.20
+// };
+// print(StudentInfo["Roll"]);
+// print(StudentInfo);
+
+// Add New Data
+
+Map <String, dynamic> StInfo = {
+  "Name":"Razwan Ahammad",
   "Age" : 22,
   "Roll" : 93,
   "Department ":"CST",
-  "CGPA" : 3.20
-};
-print(StudentInfo["Roll"]);
-print(StudentInfo);
+  "CGPA" : 3.20 };
+
+  print(StInfo["Age"]);
+  print(StInfo["Name"]);
+ StInfo["Country"] = "Bangladesh";
+ print(StInfo["Country"]);
 
 }
