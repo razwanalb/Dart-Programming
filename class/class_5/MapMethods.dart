867f94dd593c79adfe4d4b57896ed8bcf9bdfe06
Razwan Ahammad <razwan.self@gmail.com>
2026-08-12 18:51:15 +0600

    Practice Map Methods

--- class/class_5/MapMethods.dart
+++ class/class_5/MapMethods.dart
@@ -1,35 +1,52 @@
-import '../class_3/named_Optional.dart';
 
 void main(){
 
   
-  Map<String, dynamic> product = {
-  "name": "Keyboard",
-  "price": 1500,
-  "quantity": 2
-};
-// clear 
-//product.clear();
-product["country"] = "Bangladesh";
-product["price"] = 1800;
-// remove 
-product.remove("price"); // remove()
-print(product["name"]);
-print(product["price"]);
-print(product["quantity"]);
-print(product["country"]);
-// keys 
-print(product.containsKey("name"));
-print(product.containsKey("price"));
-print((product.containsKey("quantity")));
+//   Map<String, dynamic> product = {
+//   "name": "Keyboard",
+//   "price": 1500,
+//   "quantity": 2
+// };
+// // clear 
+// //product.clear();
+// product["country"] = "Bangladesh";
+// product["price"] = 1800;
+// // remove 
+// product.remove("price"); // remove()
+// print(product["name"]);
+// print(product["price"]);
+// print(product["quantity"]);
+// print(product["country"]);
+// // keys 
+// print(product.containsKey("name"));
+// print(product.containsKey("price"));
+// print((product.containsKey("quantity")));
 
-// values 
-print(product.containsValue(1800));
-print(product.containsValue(1000));
-//print(product); // clear()
-print(product.length);
-print(product.keys);
-print(product.values);
+// // values 
+// print(product.containsValue(1800));
+// print(product.containsValue(1000));
+// //print(product); // clear()
+// print(product.length);
+// print(product.keys);
+// print(product.values);
+
+
+Map<String, dynamic> product = {
+  "name": "Keyboard",
+  "price": 1800,
+  "quantity": 2,
+  "brand": "Logitech"
+};
+
+for(dynamic myProduct in product.entries){
+
+}
+  print(product.containsKey("price"));
+  print(product.containsKey("Mouse"));
+  product.remove("brand");
+  print(product.length);
+  print(product.keys);
+  print(product.values);
 
 
 }
