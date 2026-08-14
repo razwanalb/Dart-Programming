d87278256b4a019754fc51260e29030e8a19e69e
Razwan Ahammad <razwan.self@gmail.com>
2026-08-14 23:44:15 +0600

    Map Loops

--- class/class_5/MapLoops.dart
+++ class/class_5/MapLoops.dart
@@ -1,3 +1,18 @@
 void main(){
-  
+  Map<String, dynamic> product = {
+  "name": "Keyboard",
+  "price": 1800,
+  "quantity": 2,
+  "brand": "Logitech"
+};
+
+print(product["name"]);
+print(product["price"]);
+print(product["quantity"]);
+print(product["brand"]);
+
+for (var key in product.keys) {
+  print(key);
+}
+
 }
